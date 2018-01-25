9e9ad52e51e19023396fb1754284f11f92bf154b
msvprogs <msv_box@mail.ru>
2018-01-25 17:49:28 +0300

    +

--- Msv.AutoMiner/Msv.AutoMiner.FrontEnd/Startup.cs
+++ Msv.AutoMiner/Msv.AutoMiner.FrontEnd/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Msv.AutoMiner.Common.External;
 using Msv.AutoMiner.Common.External.Contracts;
 using Msv.AutoMiner.Common.Infrastructure;
+using Msv.AutoMiner.Common.ServiceContracts;
 using Msv.AutoMiner.Data;
 using Msv.AutoMiner.Data.Logic;
 using Msv.AutoMiner.FrontEnd.Infrastructure;
@@ -90,6 +91,9 @@ namespace Msv.AutoMiner.FrontEnd
             services.AddSingleton<IOverallProfitabilityCalculator, OverallProfitabilityCalculator>();
             services.AddSingleton<IControlCenterService>(x => new ControlCenterServiceClient(
                 new AsyncRestClient(new Uri(Configuration["Services:ControlCenter:Url"]))));
+            services.AddSingleton<ICoinInfoService>(x => new CoinInfoServiceClient(
+                new AsyncRestClient(new Uri(Configuration["Services:CoinInfo:Url"])),
+                Configuration["Services:CoinInfo:ApiKey"]));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
