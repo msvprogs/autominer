1e507b40072139be860f432463ff6f4f55ee4710
msvprogs <msv_box@mail.ru>
2018-01-28 16:13:53 +0300

    Added required field markers

---
+++ Msv.AutoMiner/Msv.AutoMiner.FrontEnd/Infrastructure/RequiredFieldMarkerTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Msv.AutoMiner.FrontEnd.Infrastructure
+{
+    [HtmlTargetElement("label", Attributes = "asp-for")]
+    public class RequiredFieldMarkerTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (For == null || !For.Metadata.IsRequired)
+                return;
+
+            var asteriskSpan = new TagBuilder("span")
+            {
+                Attributes =
+                {
+                    ["class"] = "required-value-marker",
+                    ["title"] = "This field is required"
+                }
+            };
+            asteriskSpan.InnerHtml.Append("*");
+            output.PreContent.AppendHtml(asteriskSpan);
+        }
+    }
+}
