55d2e9121c208706553a948aa406f0064dc16c13
msvprogs <msv_box@mail.ru>
2018-01-31 11:20:47 +0300

    Revert "Temp: Returned synchronous methods of WebClient"
    This reverts commit f471540a01e810bfba0534ae2daae9c22d1559d8.

--- Msv.AutoMiner/Msv.HttpTools/CorrectWebClient.cs
+++ Msv.AutoMiner/Msv.HttpTools/CorrectWebClient.cs
@@ -41,8 +41,7 @@ namespace Msv.HttpTools
                 throw new ArgumentNullException(nameof(headers));
 
             SetHeaders(headers);
-            return Task.FromResult(DownloadString(uri));
-            //return DoTaskWithTimeoutAsync(DownloadStringTaskAsync(uri));
+            return DoTaskWithTimeoutAsync(DownloadStringTaskAsync(uri));
         }
 
         public Task<string> DownloadStringAsync(Uri uri, Dictionary<HttpRequestHeader, string> headers)
@@ -53,8 +52,7 @@ namespace Msv.HttpTools
                 throw new ArgumentNullException(nameof(headers));
 
             SetHeaders(headers);
-            return Task.FromResult(DownloadString(uri));
-            //return DoTaskWithTimeoutAsync(DownloadStringTaskAsync(uri));
+            return DoTaskWithTimeoutAsync(DownloadStringTaskAsync(uri));
         }
 
         public Task<string> UploadStringAsync(Uri uri, string data, Dictionary<string, string> headers, NetworkCredential credentials = null)
@@ -66,8 +64,7 @@ namespace Msv.HttpTools
 
             SetHeaders(headers);
             Credentials = credentials;
-            return Task.FromResult(UploadString(uri,data));
-            //return DoTaskWithTimeoutAsync(UploadStringTaskAsync(uri, data));
+            return DoTaskWithTimeoutAsync(UploadStringTaskAsync(uri, data));
         }
 
         public Task<string> UploadStringAsync(Uri uri, string data, Dictionary<HttpRequestHeader, string> headers, NetworkCredential credentials = null)
@@ -79,8 +76,7 @@ namespace Msv.HttpTools
 
             SetHeaders(headers);
             Credentials = credentials;
-            return Task.FromResult(UploadString(uri, data));
-            //return DoTaskWithTimeoutAsync(UploadStringTaskAsync(uri, data));
+            return DoTaskWithTimeoutAsync(UploadStringTaskAsync(uri, data));
         }
 
         protected override WebRequest GetWebRequest(Uri address)
@@ -114,20 +110,20 @@ namespace Msv.HttpTools
             Headers[HttpRequestHeader.AcceptEncoding] = AcceptEncodings;
         }
 
-        //private async Task<string> DoTaskWithTimeoutAsync(Task<string> task)
-        //{
-        //    var timeoutCancelSource = new CancellationTokenSource();
-        //    var timeoutTask = Task.Delay(M_MaxRequestTimeout, timeoutCancelSource.Token);
+        private async Task<string> DoTaskWithTimeoutAsync(Task<string> task)
+        {
+            var timeoutCancelSource = new CancellationTokenSource();
+            var timeoutTask = Task.Delay(M_MaxRequestTimeout, timeoutCancelSource.Token);
 
-        //    var resultTask = await Task.WhenAny(task, timeoutTask);
-        //    if (resultTask == timeoutTask)
-        //    {
-        //        CancelAsync();
-        //        throw new TimeoutException("WebClient operation timed out. All default timeouts were ignored (probably a .NET Core implementation bug)");
-        //    }
+            var resultTask = await Task.WhenAny(task, timeoutTask);
+            if (resultTask == timeoutTask)
+            {
+                CancelAsync();
+                throw new TimeoutException("WebClient operation timed out. All default timeouts were ignored (probably a .NET Core implementation bug)");
+            }
 
-        //    timeoutCancelSource.Cancel();
-        //    return await task;
-        //}
+            timeoutCancelSource.Cancel();
+            return await task;
+        }
     }
 }
