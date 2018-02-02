c3c164af04bba80c7e1597b61779e32440ce8e36
msvprogs <msv_box@mail.ru>
2018-02-02 23:00:33 +0300

    +

--- Msv.AutoMiner/Msv.AutoMiner.ControlCenterService/External/PoolInfoProviders/OpenEthereumPoolInfoProvider.cs
+++ Msv.AutoMiner/Msv.AutoMiner.ControlCenterService/External/PoolInfoProviders/OpenEthereumPoolInfoProvider.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using Msv.AutoMiner.Common.External.Contracts;
 using Msv.AutoMiner.Common.Helpers;
 using Msv.AutoMiner.ControlCenterService.External.Contracts;
 using Msv.AutoMiner.ControlCenterService.External.Data;
+using Msv.HttpTools;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -28,16 +30,7 @@ namespace Msv.AutoMiner.ControlCenterService.External.PoolInfoProviders
         }
 
         public PoolInfo GetInfo(DateTime minPaymentDate)
-        {
-            dynamic accountJson = JsonConvert.DeserializeObject(
-                m_WebClient.DownloadString(m_Url + "/api/accounts/" + m_Wallet));
-            var accountInfo = new PoolAccountInfo
-            {
-                HashRate = (long) accountJson.currentHashrate,
-                ValidShares = (int) (accountJson.roundShares / 1e9),
-                ConfirmedBalance = (double) accountJson.stats.balance / 1e9,
-                UnconfirmedBalance = (double) accountJson.stats.immature / 1e9
-            };
+        {            
             dynamic stateJson = JsonConvert.DeserializeObject(
                 m_WebClient.DownloadString(m_Url + "/api/stats"));
             var state = new PoolState
@@ -46,24 +39,47 @@ namespace Msv.AutoMiner.ControlCenterService.External.PoolInfoProviders
                 LastBlock = (long) stateJson.stats.lastBlockFound,
                 TotalHashRate = (long) stateJson.hashrate
             };
-            var payments = accountJson.payments != null
-                ? ((JArray) accountJson.payments)
-                .Cast<dynamic>()
-                .Select(x => new PoolPaymentData
-                {
-                    Amount = (double) x.amount / 1e9,
-                    DateTime = DateTimeHelper.ToDateTimeUtc((long) x.timestamp),
-                    Transaction = (string) x.tx
-                })
-                .Where(x => x.DateTime > minPaymentDate)
-                .ToArray()
-                : new PoolPaymentData[0];
-            return new PoolInfo
+
+            try
             {
-                AccountInfo = accountInfo,
-                PaymentsData = payments,
-                State = state
-            };
+                dynamic accountJson = JsonConvert.DeserializeObject(
+                    m_WebClient.DownloadString(m_Url + "/api/accounts/" + m_Wallet));
+                var accountInfo = new PoolAccountInfo
+                {
+                    HashRate = (long) accountJson.currentHashrate,
+                    ValidShares = (int) (accountJson.roundShares / 1e9),
+                    ConfirmedBalance = (double) accountJson.stats.balance / 1e9,
+                    UnconfirmedBalance = (double) accountJson.stats.immature / 1e9
+                };
+                var payments = accountJson.payments != null
+                    ? ((JArray) accountJson.payments)
+                    .Cast<dynamic>()
+                    .Select(x => new PoolPaymentData
+                    {
+                        Amount = (double) x.amount / 1e9,
+                        DateTime = DateTimeHelper.ToDateTimeUtc((long) x.timestamp),
+                        Transaction = (string) x.tx
+                    })
+                    .Where(x => x.DateTime > minPaymentDate)
+                    .ToArray()
+                    : new PoolPaymentData[0];
+                return new PoolInfo
+                {
+                    AccountInfo = accountInfo,
+                    PaymentsData = payments,
+                    State = state
+                };
+            }
+            catch (CorrectHttpException ex) when (ex.Status == HttpStatusCode.NotFound)
+            {
+                //Wallet not found
+                return new PoolInfo
+                {
+                    AccountInfo = new PoolAccountInfo(),
+                    PaymentsData = new PoolPaymentData[0],
+                    State = state
+                };
+            }
         }
     }
 }
