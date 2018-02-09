1f8d1ac53445cee17e4fd17383786fd120f0799b
msvprogs <msv_box@mail.ru>
2018-02-09 17:44:33 +0300

    Revert "Added obfuscation to control center service"
    This reverts commit e1caaaee1c19909d081392fd9d5b0868991f6bdc.

--- Msv.AutoMiner/Msv.BrowserCheckBypassing/CloudflareBrowserCheckBypasser.cs
+++ Msv.AutoMiner/Msv.BrowserCheckBypassing/CloudflareBrowserCheckBypasser.cs
@@ -23,7 +23,7 @@ namespace Msv.BrowserCheckBypassing
         static CloudflareBrowserCheckBypasser()
         {
             using (var jsStream = typeof(CloudflareBrowserCheckBypasser).Assembly
-                .GetManifestResourceStream("Msv.BrowserCheckBypassing.cloudflare.js"))
+                .GetManifestResourceStream(typeof(CloudflareBrowserCheckBypasser), "cloudflare.js"))
             using (var reader = new StreamReader(jsStream))
                 M_InitJs = reader.ReadToEnd();
         }
