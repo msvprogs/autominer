42e96ec3de17b0bf6cafac5b65055f3fac0a100e
msvprogs <msv_box@mail.ru>
2018-06-07 21:57:37 +0300

    Added block reward verification for ZEC

--- Msv.AutoMiner/Msv.AutoMiner.NetworkInfo/Specific/ZcashInfoProvider.cs
+++ Msv.AutoMiner/Msv.AutoMiner.NetworkInfo/Specific/ZcashInfoProvider.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using Msv.AutoMiner.Common.External.Contracts;
 using Msv.AutoMiner.Common.Helpers;
 using Msv.AutoMiner.NetworkInfo.Data;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Msv.AutoMiner.NetworkInfo.Specific
 {
@@ -10,7 +12,7 @@ namespace Msv.AutoMiner.NetworkInfo.Specific
     [SpecificCoinInfoProvider("ZEC")]
     public class ZcashInfoProvider : INetworkInfoProvider
     {
-        private static readonly Uri M_BaseUri = new Uri("https://api.zcha.in");
+        private static readonly Uri M_BaseUri = new Uri("https://api.zcha.in/v2/mainnet/");
         private static readonly Uri M_ExplorerBaseUri = new Uri("https://explorer.zcha.in/");
 
         private readonly IWebClient m_WebClient;
@@ -21,19 +23,33 @@ namespace Msv.AutoMiner.NetworkInfo.Specific
         public CoinNetworkStatistics GetNetworkStats()
         {
             dynamic networkInfo = JsonConvert.DeserializeObject(
-                m_WebClient.DownloadString(new Uri(M_BaseUri, "/v2/mainnet/network")));
+                m_WebClient.DownloadString(new Uri(M_BaseUri, "network")));
             dynamic lastBlockInfo = JsonConvert.DeserializeObject(
-                m_WebClient.DownloadString(new Uri(M_BaseUri, "/v2/mainnet/blocks/" + (string) networkInfo.blockHash)));
+                m_WebClient.DownloadString(new Uri(M_BaseUri, "blocks/" + (string) networkInfo.blockHash)));
+            var lastBlockTransactions = JsonConvert.DeserializeObject<JArray>(
+                m_WebClient.DownloadString(new Uri(M_BaseUri,
+                    $"blocks/{(string) networkInfo.blockHash}/transactions?limit=20&offset=0&sort=index&direction=ascending")));
             return new CoinNetworkStatistics
             {
                 Difficulty = (double) networkInfo.difficulty,
                 NetHashRate = (long) networkInfo.hashrate,
                 BlockTimeSeconds = (double) networkInfo.meanBlockTime,
                 Height = (long) networkInfo.blockNumber,
-                LastBlockTime = DateTimeHelper.ToDateTimeUtc((long)lastBlockInfo.timestamp)
+                LastBlockTime = DateTimeHelper.ToDateTimeUtc((long) lastBlockInfo.timestamp),
+                LastBlockTransactions = lastBlockTransactions
+                    .Cast<dynamic>()
+                    .Select(x => new TransactionInfo
+                    {
+                        Fee = (double) x.fee,
+                        InValues = (string) x.type == "minerReward"
+                            ? new double[0]
+                            : new double[] {x.value},
+                        OutValues = new double[] {x.outputValue}
+                    })
+                    .ToArray()
             };
         }
-        
+
         public Uri CreateTransactionUrl(string hash)
             => new Uri(M_ExplorerBaseUri, $"transactions/{hash}");
 
